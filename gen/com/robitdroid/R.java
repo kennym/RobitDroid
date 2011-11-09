bcdf65161229372e2f26dbbc2a8bcf31b852f362
Kenny Meyer <knny.myer@gmail.com>
2011-11-09 18:56:56 -0300

    Show final screen.
    Robit shows the guessed answer now. :-)

--- gen/com/robitdroid/R.java
+++ gen/com/robitdroid/R.java
@@ -10,22 +10,28 @@ package com.robitdroid;
 public final class R {
     public static final class attr {
     }
+
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int icon = 0x7f020000;
     }
+
     public static final class id {
-        public static final int btn_no=0x7f050002;
-        public static final int btn_yes=0x7f050001;
-        public static final int gridview=0x7f050000;
+        public static final int btn_no = 0x7f050003;
+        public static final int btn_yes = 0x7f050002;
+        public static final int final_number = 0x7f050000;
+        public static final int gridview = 0x7f050001;
     }
+
     public static final class layout {
-        public static final int game_screen=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int final_screen = 0x7f030000;
+        public static final int game_screen = 0x7f030001;
+        public static final int main = 0x7f030002;
     }
+
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int no=0x7f040003;
-        public static final int self_start=0x7f040001;
-        public static final int yes=0x7f040002;
+        public static final int app_name = 0x7f040000;
+        public static final int no = 0x7f040003;
+        public static final int self_start = 0x7f040001;
+        public static final int yes = 0x7f040002;
     }
 }
