3dac7df8775e8d70bd93d63c3583bad1714ca6fd
Kenny Meyer <knny.myer@gmail.com>
2011-11-10 22:02:57 -0300

    Show instructions dialog... text has to be added

--- src/com/robitdroid/RobitDroid.java
+++ src/com/robitdroid/RobitDroid.java
@@ -1,6 +1,9 @@
 package com.robitdroid;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,5 +31,25 @@ public class RobitDroid extends Activity {
         // Start new game
         Intent i = new Intent(view.getContext(), GameScreen.class);
         startActivity(i);
+    }
+
+    public void showInstructions(View view) {
+        showDialog(0);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case 0:
+                return new AlertDialog.Builder(this)
+                        .setIcon(R.drawable.icon)
+                        .setTitle("Instructions")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                //Toast.makeText(getBaseContext(), "OK clicked!", Toast.LENGTH_SHORT).show();
+                            }
+                        }).create();
+        }
+        return null;
     }
 }
